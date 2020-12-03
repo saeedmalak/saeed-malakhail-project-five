213e672613260ac68eb6f5d36bd629ae191df494
saeedmalak <makhh2001@gmail.com>
2020-12-03 10:47:54 -0500

    added sweet alerts to app :sweet:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import swal from "sweetalert";
 import Ticket from "./Ticket";
 import firebase from "./firebase";
 import "./App.css";
@@ -38,6 +39,12 @@ class App extends Component {
         ticketsArray.push(formattedObj);
       }
 
+      if (ticketsArray.length === 0) {
+        // how do i write some JSX here ie. append <p> to say "Congrats! You're on a roll, no ticket enquiries to deal with!"
+
+        alert("empty page");
+      }
+
       console.log(ticketsArray);
 
       this.setState({
@@ -64,7 +71,7 @@ class App extends Component {
     });
   };
 
-  onSubmitTimestamp () {
+  onSubmitTimestamp() {
     let timestamp = new Date();
     let formatDate = [];
 
@@ -75,7 +82,19 @@ class App extends Component {
     let minutes = timestamp.getMinutes();
     let seconds = timestamp.getSeconds();
 
-    formatDate = [month, "/", day, "/", year, " ", hour, ":", minutes, ":", seconds];
+    formatDate = [
+      month,
+      "/",
+      day,
+      "/",
+      year,
+      " ",
+      hour,
+      ":",
+      minutes,
+      ":",
+      seconds,
+    ];
     console.log(formatDate);
 
     this.state.ticketDate = formatDate;
@@ -83,32 +102,56 @@ class App extends Component {
     this.setState({
       ticketDate: formatDate,
     });
-  };
+  }
 
   onSubmit = (event) => {
     event.preventDefault();
 
-    const dbRef = firebase.database().ref();
+    swal({
+      title: "Submit Ticket Request?",
+      text:
+        "Once you hit yes, you will NOT be able to go back and make any changes!",
+      icon: "warning",
+      buttons: [true, "Yes"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        const dbRef = firebase.database().ref();
 
-    this.onSubmitTimestamp();
+        this.onSubmitTimestamp();
 
-    const rawObj = {
-      name: this.state.ticketRequester,
-      description: this.state.ticketDescription,
-      priority: this.state.ticketPriority,
-      date: this.state.ticketDate,
-    };
+        const rawObj = {
+          name: this.state.ticketRequester,
+          description: this.state.ticketDescription,
+          priority: this.state.ticketPriority,
+          date: this.state.ticketDate,
+        };
+        
+        dbRef.push(rawObj);
 
-    console.log(rawObj);
+        swal(`Thank you ${rawObj.name}! Your request has been saved and someone will reach out to you in 24 hours.`, {
+          icon: "success",
+          buttons: false,
+          timer: 2500,
+        });
 
-    dbRef.push(rawObj);
+        this.setState({
+          ticketRequester: "",
+          ticketDescription: "",
+          ticketPriority: "",
+          ticketDate: "",
+        });
 
-    this.setState({
-      ticketRequester: "",
-      ticketDescription: "",
-      ticketPriority: "",
-      ticketDate: "",
+      } else {
+        swal(`No worries ${this.state.ticketRequester}, submit when your ready!`,
+          {
+            buttons: false,
+            timer: 2000,
+          }
+        );
+      }
     });
+
   };
 
   render() {
@@ -116,6 +159,55 @@ class App extends Component {
     return (
       <div className="App wrapper">
         <h1>Create New Ticket Request</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="newRequester">Requester: </label>
+          <input
+            type="text"
+            id="newRequester"
+            onChange={this.onChangeTicketRequester}
+            value={this.state.ticketRequester}
+          />
+          <label htmlFor="newDescription">Description: </label>
+          <textarea
+            name="newDescription"
+            rows="4"
+            id="newDescription"
+            onChange={this.onChangeTicketDescription}
+            value={this.state.ticketDescription}
+          ></textarea>
+          <div className="priority-container">
+            <p>Priority</p>
+            <div className="priority-items">
+              <input
+                type="radio"
+                id="newPriorityLow"
+                value="Low"
+                onChange={this.onChangeTicketPriority}
+                checked={this.state.ticketPriority === "Low"}
+              />
+              <label htmlFor="newPriorityLow">Low</label>
+              <input
+                type="radio"
+                id="newPriorityMedium"
+                value="Medium"
+                onChange={this.onChangeTicketPriority}
+                checked={this.state.ticketPriority === "Medium"}
+              />
+              <label htmlFor="newPriorityMedium">Medium</label>
+              <input
+                type="radio"
+                id="newPriorityHigh"
+                value="High"
+                onChange={this.onChangeTicketPriority}
+                checked={this.state.ticketPriority === "High"}
+              />
+              <label htmlFor="newPriorityHigh">High</label>
+            </div>
+          </div>
+
+          <button>Submit</button>
+        </form>
 
         <table className="rwd-table">
           <thead>
@@ -133,48 +225,6 @@ class App extends Component {
             })}
           </tbody>
         </table>
-
-        <form action="">
-          <label htmlFor="newRequester">Requester: </label>
-          <input
-            type="text"
-            id="newRequester"
-            onChange={this.onChangeTicketRequester}
-            value={this.state.ticketRequester}
-          />
-          <label htmlFor="newDescription">Description: </label>
-          <input
-            type="text"
-            id="newDescription"
-            onChange={this.onChangeTicketDescription}
-            value={this.state.ticketDescription}
-          />
-          <label htmlFor="newPriorityLow">Low</label>
-          <input
-            type="radio"
-            id="newPriorityLow"
-            value="Low"
-            onChange={this.onChangeTicketPriority}
-            checked={this.state.ticketPriority === "Low"}
-          />
-          <label htmlFor="newPriorityMedium">Medium</label>
-          <input
-            type="radio"
-            id="newPriorityMedium"
-            value="Medium"
-            onChange={this.onChangeTicketPriority}
-            checked={this.state.ticketPriority === "Medium"}
-          />
-          <label htmlFor="newPriorityHigh">High</label>
-          <input
-            type="radio"
-            id="newPriorityHigh"
-            value="High"
-            onChange={this.onChangeTicketPriority}
-            checked={this.state.ticketPriority === "High"}
-          />
-          <button onClick={this.onSubmit}>Submit</button>
-        </form>
       </div>
     );
   }
